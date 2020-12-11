a147aaa9fa38a4d7dbac70ca8dc4266f128100cc
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2020-12-11 08:41:54 +0500

    Main widgets have been updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quizzler/question.dart';
 
 void main() => runApp(Quizzler());
 
@@ -37,6 +38,9 @@ class _QuizPageState extends State<QuizPage> {
     true,
     true,
   ];
+
+  Question q1 = Question(
+      q: 'You can lead a cow down stairs but not up stairs.', a: false);
   int QuestionNumer = 0;
 
   @override
---
+++ lib/question.dart
@@ -0,0 +1,9 @@
+class Question {
+  String questionText;
+  bool questionAnswers;
+
+  Question({String q, bool a}) {
+    questionText = q;
+    questionAnswers = a;
+  }
+}
